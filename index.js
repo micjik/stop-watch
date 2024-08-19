4c78d50940536c8e52b163b4b56a8246b2df02b4
Michael Adetayo <micjik50@gmail.com>
2024-08-19 06:55:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let timer = document.querySelector('.timer')
+let start = document.querySelector('.start')
+let stop = document.querySelector('.stop')
+let reset = document.querySelector('.reset')
+
+let s = 0;
+let ms = 0;
+let m = 0;
+
+let counter;
